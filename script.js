f835bdf8aea9e1a58ba58ffba72acbdebd3a889e
Parsa <mohammadkazemi.parsa@gmail.com>
2022-09-11 16:27:02 +0430

    main script added

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 let playBtn = document.querySelector(".description .buttons button#play");
-let playBtn_icon = document.querySelector(".description .buttons button#play i")
+let playBtn_icon = document.querySelector(
+  ".description .buttons button#play i"
+);
 let timeTxt = document.querySelector(".time span");
 let timebarState = document.querySelector(".music-bar-time");
 let circle = document.querySelector(".bar span");
 let audiofile = document.querySelector("audio");
 let playing = false;
 
-function PlayMusic(){
-    if(playing == false){
-        playBtn_icon.className = "fas fa-pause";
-        audiofile.play();
-        playing = true;
+function PlayMusic() {
+  if (playing == false) {
+    playBtn_icon.className = "fas fa-pause";
+    audiofile.play();
+    playing = true;
+  } else {
+    playBtn_icon.className = "fas fa-play";
+    audiofile.pause();
+    playing = false;
+  }
+
+}
+
+function SetTime(){
+    let seconds = Math.floor(audiofile.currentTime%60);
+    let minutes = Math.floor(audiofile.currentTime / 60);
+
+    if(seconds < 10){
+        seconds = "0" + seconds
     }
-    else{
-        playBtn_icon.className = "fas fa-play";
-        audiofile.pause();
-        playing = false;
+    if(minutes < 10){
+        minutes = "0" + minutes
     }
+
+    timeTxt.textContent = minutes + ":" + seconds;
+
+    timebarState.style.width = (audiofile.currentTime * 100 / audiofile.duration) + "%";
+    circle.style.left = (audiofile.currentTime * 100 / audiofile.duration) + "%";
 }
 
 
+
+audiofile.addEventListener("timeupdate", SetTime)
 playBtn.addEventListener("click", PlayMusic);
-
-
